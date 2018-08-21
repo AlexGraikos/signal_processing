db5ea78f31c98d7cc723b5ee1d79897519bae258
Alex Grecos <hotogainc3@hotmail.com>
2018-08-21 22:53:34 +0300

    Fixed freq scale in continuous wavelet transform.

--- code/wavelet_transform.m
+++ code/wavelet_transform.m
@@ -25,11 +25,21 @@ end
 % Plot CWT
 figure();
 sp(1) = subplot(2,1,1);
-imagesc([0 N],[freq(1) freq(end)], abs(wt));
+imagesc(0:N-1,log2(freq),abs(wt));
 title(['Wavelet Transform ' wname ' wavelet params=' mat2str(params)]);
 xlabel('Samples');
 ylabel('Frequency');
 set(gca,'Ydir','Normal');
+Yticks = 2.^(round(log2(min(freq))):round(log2(max(freq))));
+AX = gca;
+AX.YLim = log2([min(freq), max(freq)]);
+AX.YTick = log2(Yticks);
+AX.YDir = 'normal';
+set(AX,'YLim',log2([min(freq),max(freq)]), ...
+    'layer','top', ...
+    'YTick',log2(Yticks(:)), ...
+    'YTickLabel',num2str(sprintf('%g\n',Yticks)), ...
+    'layer','top')
 
 % Plot signal
 sp(2) = subplot(2,1,2);
