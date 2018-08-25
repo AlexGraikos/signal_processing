60e6a7d7f4b376e027657f9108384306da094dba
Alex Grecos <hotogainc3@hotmail.com>
2018-08-25 21:04:52 +0300

    Added descriptions to report. Missing for signals 217,221

--- code/detect_rr_intervals.m
+++ code/detect_rr_intervals.m
@@ -1,4 +1,4 @@
-function detect_rr_intervals(record_number,lead)
+function detect_rr_intervals(record_number,lead,n0,n1)
 
 [sig,fs,tm,ann,anntype] = load_signal(record_number,lead);
 close gcf;
@@ -24,16 +24,15 @@ for i=1:n
 end
 rrsig = waverec(c,l,wname); % Reconstruct signal
 
-% Find minimum R-R peak distance
-wt = cwt(sig(20000:30000),'amor'); % Calculate mean distance between peaks using CWT
+% Find mean R-R peak distance
+wt = cwt(sig(end/2-10000:end/2+1),'amor'); % Calculate mean distance between peaks using CWT
 freq_peaks = sum(abs(wt(25:35,:))); % Peaks in QRS frequencies
 auto_coeffs = autocorr(freq_peaks,500); % Autocorr finds mean distance
 [m,i] = max(auto_coeffs(150:end));
 min_peak_dist = (i+149)*0.8; % 80% of interval is used as min peak distance
 
 % Locate peaks in square of reconstructed signal
-
-[peaks,peak_locs] = findpeaks(rrsig.^2,'MinPeakDistance',min_peak_dist,'MinPeakHeight',0.5);
+[peaks,peak_locs] = findpeaks(rrsig.^2,'MinPeakDistance',min_peak_dist,'MinPeakHeight',5*mean(rrsig.^2));
 
 % Plot results
 figure();
@@ -44,8 +43,9 @@ stem(peak_locs,sig(peak_locs),'x','LineStyle','none');
 hold on;
 stem(peak_true,sig(peak_true),'LineStyle','none','Color','g');
 hold off;
-title('Input signal with located R peaks');
+title(['Signal ' num2str(record_number) ' (lead ' num2str(lead) ') detected/true R peaks']);
 legend({'Signal','Detected','True'});
+
 
 sp2(2) = subplot(2,1,2);
 plot(rrsig.^2);
@@ -53,5 +53,6 @@ hold on;
 stem(peak_locs,rrsig(peak_locs).^2,'x','LineStyle','none');
 linkaxes(sp2,'x');
 title('Reconstructed signal');
+
 
 end
